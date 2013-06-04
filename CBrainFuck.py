53b3f9d5484917411c09854f81392029ba36eb28
nzt4567 <nzt4567@gmx.com>
2013-06-04 23:18:04 +0200

    Finished: v1

--- CBrainFuck.py
+++ CBrainFuck.py
@@ -1,8 +1,45 @@
+#!/usr/bin/env python
+
+'''
+  Brainfuck (http://en.wikipedia.org/wiki/Brainfuck) programming language
+  interpreter published under GNU GPL v3.
+
+  Author: nzt4567 (nzt4567 (at) gmx (dot) com)                 Year: 2013
+'''
+
 # IMPORTS
-import re, os.path, sys
+import os.path, sys, argparse
+
+# CREDITS
+__author__  = "nzt4567"
+__email__   = "nzt4567@gmx.com"
+__status__  = "Completed"
+__version__ = "0.1"
+__license__ = "GNU GPL v3"
+__year__    = "2013"
+
+############################ STANDARD HELPERS ###########################
+def pa():
+  ''' Parse arguments and return them '''
+  
+  p = argparse.ArgumentParser(description='''Brainfuck programming '''  +
+  '''language interpreter. Requires Python 3.3''',
+  epilog='''Created by: ''' + __author__ + '''; License: ''' + 
+  __license__ + '''; Year: ''' + __year__ + '''; Please report ''' +
+  '''bugs (bugs, what's that?) to ''' + __email__)
+
+  # source
+  p.add_argument('source', help='''path to program source file or ''' +
+    '''the source code itself''')
+  
+  # -V / --version
+  p.add_argument('-V', '--version', action='version',version='%(prog)s '+
+                  __version__, help='''print program version and exit''')
+  
+  return vars(p.parse_args())
 
 def exit_failure(code_, err_=None):
-  ''' Exit with printing error message (if any) and correct exit code. '''
+  ''' Exit with printing error message (if any) and correct exit code '''
 
   ec = { "INVALID_CODE": 1,
          "BF_PROCESS_INPUT": 10,
@@ -16,13 +53,14 @@ def exit_failure(code_, err_=None):
 
   exit(ec[code_])
 
-def create_error_msg(type_, value_, ignorable_=True):
-  ''' Create and return error message in universal format. '''
+def create_error_msg(type_, value_, ignorable_=False):
+  ''' Create and return error message in universal format '''
 
   err_types = { "WRONG_TYPE": "internal; wrong error type",
                 "INTERNAL": "internal; general internal error",
                 "PYTHON": "original python exception text",
-                "EMPTY_SOURCE": "source file/string does not seem to contain instructions",
+                "EMPTY_SOURCE": "source file/string does not seem " +
+                                "to contain instructions",
                 "PARSING": "source code contains errors in loops" }
 
   if ignorable_ == True:
@@ -37,14 +75,18 @@ def create_error_msg(type_, value_, ignorable_=True):
   err_type = err_types[type_]
   err_value = value_
   
-  return "[ERROR] {0}: {1}: {2}: \'{3}\'".format(err_prog, err_level, err_type, err_value)
+  return "[ERROR] {0}: {1}: {2}: \'{3}\'".format(err_prog, err_level, \
+                                                 err_type, err_value)
 
+
+
+############################ BF INTERPRETER #############################
 class BrainFuck():
-  ''' BrainFuck programming language interpreter. '''
+  ''' BrainFuck programming language interpreter '''
  
-# INSTRUCTIONS
+############################ INSTRUCTIONS ###############################
   def i_memInc(self, mem_, p_mem_, code_, ip_, out_):
-    ''' Execute > instruction. '''
+    ''' Execute > instruction '''
     
     p_mem_ += code_[ip_][1]
     if len(mem_) <= p_mem_:
@@ -53,7 +95,7 @@ class BrainFuck():
     return ip_ + code_[ip_][1], p_mem_
 
   def i_memDec(self, mem_, p_mem_, code_, ip_, out_):
-    ''' Execute < instruction. '''
+    ''' Execute < instruction '''
     
     p_mem_ -= code_[ip_][1]
     if p_mem_ < 0:
@@ -62,19 +104,19 @@ class BrainFuck():
     return ip_ + code_[ip_][1], p_mem_
 
   def i_valInc(self, mem_, p_mem_, code_, ip_, out_):
-    ''' Execute + instruction. '''
+    ''' Execute + instruction '''
     
     mem_[p_mem_] = (mem_[p_mem_] + code_[ip_][1]) % 256
     return ip_ + code_[ip_][1], p_mem_
 
   def i_valDec(self, mem_, p_mem_, code_, ip_, out_):
-    ''' Execute - instruction. '''
+    ''' Execute - instruction '''
 
     mem_[p_mem_] = (mem_[p_mem_] - code_[ip_][1]) % 256
     return ip_ + code_[ip_][1], p_mem_
 
   def i_write(self, mem_, p_mem_, code_, ip_, out_):
-    ''' Execute . instruction. '''
+    ''' Execute . instruction '''
     
     out_.append(chr(mem_[p_mem_]) * code_[ip_][1])
     sys.stdout.write(chr(mem_[p_mem_]) * code_[ip_][1])
@@ -82,9 +124,8 @@ class BrainFuck():
     return ip_ + code_[ip_][1], p_mem_
 
   def i_read(self, mem_, p_mem_, code_, ip_, out_):
-    ''' Execute , instruction. '''
+    ''' Execute , instruction '''
     
-    # This reading from my stdin is not properly tested
     if 'stdin' in code_:
       i = 0
       while len(code_['stdin']) >= 1 and i < code_[ip_][1]:
@@ -98,7 +139,7 @@ class BrainFuck():
     return ip_ + code_[ip_][1], p_mem_
 
   def i_loopBeg(self, mem_, p_mem_, code_, ip_, out_):
-    ''' Execute [ instruction. '''
+    ''' Execute [ instruction '''
     
     if mem_[p_mem_] == 0:
       return code_[ip_][1], p_mem_
@@ -106,14 +147,14 @@ class BrainFuck():
     return ip_ + 1, p_mem_
 
   def i_loopEnd(self, mem_, p_mem_, code_, ip_, out_):
-    ''' Execute ] instruction. '''
+    ''' Execute ] instruction '''
     
     if mem_[p_mem_] != 0:
       return code_[ip_][1], p_mem_
 
     return ip_ + 1, p_mem_ 
 
-# VARIABLES
+############################# INSTRUCTION SET ###########################
   instruction_set = { '>': i_memInc,
                       '<': i_memDec,
                       '+': i_valInc,
@@ -123,18 +164,18 @@ class BrainFuck():
                       '[': i_loopBeg,
                       ']': i_loopEnd }
 
-# OTHERS
+######################### PARSER && INTERPRETER #########################
   def bf2dict(self, data_):
-    ''' Convert brainfuck program into internal form for faster processing. '''
+    ''' Convert source code into internal form for faster execution '''
 
     p = dict() # Program in internal form
     s = list() # Stack for loops parsing
     L = len(data_) # Input length
     i = 0 # Helper IP
-    c = 0 # Same instruction counter
-    prev = (data_[i], i) # (prev_instr_type, start_addr)
+    c = 0 # Same instructions counter
+    prev = (data_[i], i) # Previous inst. (instr_type, instr_addr)
 
-    for i in range(0, L):
+    for i in range(0, L): # Convert source code to dict() representation
       if data_[i] in ('>', '<', '+', '-', '.', ','):
         if data_[i] == prev[0]:
           c += 1
@@ -152,7 +193,7 @@ class BrainFuck():
       elif data_[i] == ']':
         if prev[0] in ('>', '<', '+', '-', '.', ','):
           p[prev[1]] = (prev[0], c)
-        prev = (data_[i], i)        
+        prev = (data_[i], i)
         c = 1
         a = s.pop()
         p[a] = ('[', i + 1)
@@ -164,16 +205,16 @@ class BrainFuck():
 
     if prev[0] in ('>', '<', '+', '-', '.', ','):
       p[prev[1]] = (prev[0], c)
-    p[i+1] = ('@', 0) # STOP instruction; added at the end of code
+    p[i+1] = ('@', 0) # Add STOP instruction
     
     return p
 
   def process_input(self, data_):
-    ''' Parse input and return program in internal optimized form. '''
+    ''' Clean comments && separate input marked by ! from source code '''
 
-    if os.path.isfile(data_):
-      with open(data_) as f:
-        data_ = f.read()
+    if os.path.isfile(data_): # If data_ is a file, ...
+      with open(data_) as f: # ... treat it's content as source code ...
+        data_ = f.read() # ... else data_ itself is treated as source 
 
     if '!' in data_: # Strip the input if there is any
       i = data_[data_.find('!'):]
@@ -188,37 +229,39 @@ class BrainFuck():
         data_ = data_.replace(c, '')
 
     if len(data_) == 0:
-      raise ValueError(create_error_msg("EMPTY_SOURCE", "", False))
+      raise ValueError("source code seems to be empty")
 
     return self.bf2dict(data_ + i)
 
   def execute_code(self, code_, mem_, p_mem_):
-    ''' Run code in internal form. '''
+    ''' Run code in internal form '''
 
     ip = 0 # instruction pointer
     output = list() # stdout
 
     while code_[ip] != ('@', 0): # Execution stops at STOP instruction
-      ip, p_mem_ = self.instruction_set[code_[ip][0]](self, mem_, p_mem_, code_, ip, output)
+      ip, p_mem_ = self.instruction_set[code_[ip][0]](self, mem_, p_mem_,\
+                                                      code_, ip, output)
 
     return output, mem_, p_mem_
 
-# CONSTRUCTOR
-  def __init__(self, data, memory=b'\x00', memory_pointer=0):
+####################### CONSTRUCTOR == RUN PROGRAM ######################
+  def __init__(self, data_, memory_=b'\x00', memory_pointer_=0):
     ''' Parse, execute and store output of code in data. '''
 
     try: # Process the input and convert it to dict()
-      self.data = self.process_input(data)
+      self.data = self.process_input(data_)
     except (OSError, IOError, TypeError, ValueError) as e:
-      exit_failure("BF_PROCESS_INPUT", e)
+      exit_failure("BF_PROCESS_INPUT", create_error_msg("PYTHON", e))
 
     try: # Execute the programme in self.data
       self.output, self.memory, self.memory_pointer = \
-        self.execute_code(self.data, bytearray(memory), memory_pointer)
-    except (OSError, TypeError, ValueError, IndexError, KeyError) as e:
-      exit_failure("BF_EXECUTE_CODE", create_error_msg("PYTHON", e, False))
+        self.execute_code(self.data, bytearray(memory_), memory_pointer_)
+    except (OSError, TypeError, ValueError, IndexError) as e:
+      exit_failure("BF_EXECUTE_CODE", create_error_msg("PYTHON", e))
 
     self.output = ''.join(self.output)
 
-  def get_memory(self):
-    return self.memory
+if __name__ == '__main__':
+  a = pa()
+  BrainFuck(a['source'])
---
+++ README.md
@@ -0,0 +1,11 @@
+brainfuck
+=========
+
+Brainfuck/Brainloller/Braincopter programming language interpreter written in python
+
+
+So far, only the brainfuck interpreter is finished :/ Just run "python CBrainFuck.py path_to_source" OR 
+"python CBrainFuck.py source_code" and your code will get executed immediately.
+
+THIS PROGRAM COMES WITH NO WARRANTY AT ALL => I AM NOT RESPONSIBLE FOR ANYTHING THE CODE WILL DO.
+DO NOT MAIL ME THAT IT HAS DELETED ALL YOUR FILES AND YOU WANT ME TO PAY FOR THEM. I WON'T. YOU HAVE BEEN WARNED.
