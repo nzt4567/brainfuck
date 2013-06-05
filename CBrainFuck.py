cacaccd0013e47b2ba8fbce3e149b621304fbdc8
nzt4567 <nzt4567@gmx.com>
2013-06-05 02:12:02 +0200

    Divided into files, changed the way to execute the script

--- CBrainFuck.py
+++ CBrainFuck.py
@@ -1,84 +1,17 @@
 #!/usr/bin/env python
 
-'''
-  Brainfuck (http://en.wikipedia.org/wiki/Brainfuck) programming language
-  interpreter published under GNU GPL v3.
-
-  Author: nzt4567 (nzt4567 (at) gmx (dot) com)                 Year: 2013
-'''
+''' bF interpreter; author: nzt4567; year: 2013 '''
 
 # IMPORTS
-import os.path, sys, argparse
+import std, os.path, sys
 
 # CREDITS
-__author__  = "nzt4567"
-__email__   = "nzt4567@gmx.com"
-__status__  = "Completed"
-__version__ = "0.1"
-__license__ = "GNU GPL v3"
-__year__    = "2013"
-
-############################ STANDARD HELPERS ###########################
-def pa():
-  ''' Parse arguments and return them '''
-  
-  p = argparse.ArgumentParser(description='''Brainfuck programming '''  +
-  '''language interpreter. Requires Python 3.3''',
-  epilog='''Created by: ''' + __author__ + '''; License: ''' + 
-  __license__ + '''; Year: ''' + __year__ + '''; Please report ''' +
-  '''bugs (bugs, what's that?) to ''' + __email__)
-
-  # source
-  p.add_argument('source', help='''path to program source file or ''' +
-    '''the source code itself''')
-  
-  # -V / --version
-  p.add_argument('-V', '--version', action='version',version='%(prog)s '+
-                  __version__, help='''print program version and exit''')
-  
-  return vars(p.parse_args())
-
-def exit_failure(code_, err_=None):
-  ''' Exit with printing error message (if any) and correct exit code '''
-
-  ec = { "INVALID_CODE": 1,
-         "BF_PROCESS_INPUT": 10,
-         "BF_EXECUTE_CODE": 15 }
-
-  if code_ not in ec:
-    code_ = "INVALID_CODE"
-
-  if err_:
-    print(err_)
-
-  exit(ec[code_])
-
-def create_error_msg(type_, value_, ignorable_=False):
-  ''' Create and return error message in universal format '''
-
-  err_types = { "WRONG_TYPE": "internal; wrong error type",
-                "INTERNAL": "internal; general internal error",
-                "PYTHON": "original python exception text",
-                "EMPTY_SOURCE": "source file/string does not seem " +
-                                "to contain instructions",
-                "PARSING": "source code contains errors in loops" }
-
-  if ignorable_ == True:
-    err_level = "ignored error"
-  else:
-    err_level = "fatal error"
-
-  if type_ not in err_types:
-    type_ = "WRONG_TYPE"
-
-  err_prog = os.path.split(sys.argv[0])[1]
-  err_type = err_types[type_]
-  err_value = value_
-  
-  return "[ERROR] {0}: {1}: {2}: \'{3}\'".format(err_prog, err_level, \
-                                                 err_type, err_value)
-
-
+__author__  = std.__author__
+__email__   = std.__email__
+__status__  = std.__status__
+__version__ = std.__version__
+__license__ = std.__license__
+__year__    = std.__year__
 
 ############################ BF INTERPRETER #############################
 class BrainFuck():
@@ -253,16 +186,22 @@ class BrainFuck():
     try: # Process the input and convert it to dict()
       self.data = self.process_input(data_)
     except (OSError, IOError, TypeError, ValueError) as e:
-      exit_failure("BF_PROCESS_INPUT", create_error_msg("PYTHON", e))
+      std.exit_failure("BF_PROCESS_INPUT", \
+        std.create_error_msg("PYTHON", e))
 
     try: # Execute the programme in self.data
       self.output, self.memory, self.memory_pointer = \
         self.execute_code(self.data, bytearray(memory_), memory_pointer_)
     except (OSError, TypeError, ValueError, IndexError) as e:
-      exit_failure("BF_EXECUTE_CODE", create_error_msg("PYTHON", e))
+      std.exit_failure("BF_EXECUTE_CODE", \
+        std.create_error_msg("PYTHON", e))
 
     self.output = ''.join(self.output)
 
+################################# MAIN ##################################
 if __name__ == '__main__':
-  a = pa()
-  BrainFuck(a['source'])
+  if len(sys.argv) > 1:
+    BrainFuck(sys.argv[1])
+  else:
+    std.exit_failure("INVALID_CODE", "You have executed CBrainFuck.py " +
+                    "directly but without source. Better run __main__.")
